d8e1efd99f3886024cd6fdc35ef451eb794e6822
CashCalcDaniel <39903235+CashCalcDaniel@users.noreply.github.com>
2018-08-22 18:27:18 +0100

    Removed dev work

--- resources/views/team-sheet.blade.php
+++ resources/views/team-sheet.blade.php
@@ -8,47 +8,6 @@
 @endsection
 
 @section('content')
-<div class="page-container">
-    <div class="team-title fz30">
-        {{ $gameweek }}
-    </div>
-    @foreach($teams as $team)
-        <div class="team-title fz30">
-            {{ $team[7] }}
-        </div>
-        <div class="team-container">
-            <div class="player-container GK">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[0] }}</span>
-            </div>
-            <div class="player-container LB">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[1] }}</span>
-            </div>
-            <div class="player-container RB">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[2] }}</span>
-            </div>
-            <div class="player-container CM">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[3] }}</span>
-            </div>
-            <div class="player-container RM">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[4] }}</span>
-            </div>
-            <div class="player-container LM">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[5] }}</span>
-            </div>
-            <div class="player-container ST">
-                <img src="{{ asset('images/gseshirt.jpg') }}" alt="GSE Shirt" class="player-shirt">
-                <span class="player-name">{{ $team[6] }}</span>
-            </div>
-        </div>
-    @endforeach
-</div>
-
 @endsection
 
 @section('scripts')
